a278d65d6f084aa0622ae0af5f6008fd944ed644
liudecai <liudecai@postop.cn>
2018-06-22 16:55:50 +0800

    first commit

---
+++ src/main/java/cn/alittler/SpringBoot2Application.java
@@ -0,0 +1,18 @@
+package cn.alittler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动入口
+ * 
+ * @author LiuDeCai
+ * @date 2018-06-19.
+ */
+@SpringBootApplication
+public class SpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2Application.class, args);
+	}
+}
---
+++ src/test/java/cn/alittler/SpringBoot2ApplicationTests.java
@@ -0,0 +1,22 @@
+package cn.alittler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 启动入口测试
+ * 
+ * @author LiuDeCai
+ * @date 2018-06-19.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
